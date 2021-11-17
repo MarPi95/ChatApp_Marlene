3a6d11d614cdbc6932ee9f423bbf77ecb3adcf0d
Marlene Pirker <mar.pirker@hotmail.com>
2021-11-17 09:21:50 +0100

    Lesson 20 - 1

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, make_response
 from flask_sqlalchemy import SQLAlchemy
 
 webapp = Flask(__name__)
-webapp.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
+webapp.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
 webapp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 db = SQLAlchemy(webapp)
+
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=False)
     message = db.Column(db.String, unique=False)
 
+
 db.create_all()
 
-@webapp.route("/", methods=["GET", "POST"])
+
+@webapp.route("/", methods=["GET"])
 def index():
+    username = request.cookies.get("remember_user")
+
+    return render_template("index.html", username=username)
+
+
+@webapp.route("/login", methods=["Post"])
+def login():
+    username = request.form.get("username")
+    remember_me = request.form.get("remember")
+
+    messages = Message.query.all()
+    response = make_response(render_template("chat.html", username=username, messages=messages))
+    if remember_me:
+        response.set_cookie("remember_user", username)
+    else:
+        response.set_cookie("remember_user", "")
+
+    return response
+
+
+@webapp.route("/chat", methods=["GET", "POST"])
+def chat():
     username = None
     if request.method == "POST":
         username = request.form.get("username")
@@ -25,25 +52,27 @@ def index():
 
         db.session.add(message_model)
         db.session.commit()
-
-        print(f"{username} - {message}")
     else:
         username = request.args.get("username")
 
     messages = Message.query.all()
-    return render_template("index.html", username=username, messages=messages)
+
+    return render_template("chat.html", username=username, messages=messages)
+
 
 @webapp.route("/delete", methods=["GET"])
 def delete():
     message_id = request.args.get("id")
+
     print("delete {}".format(message_id))
 
     db.session.query(Message).filter(Message.id == message_id).delete()
-    db.session.commit
+    db.session.commit()
 
     username = request.args.get("username")
     messages = Message.query.all()
-    return render_template("index.html", username=username, messages=messages)
+
+    return render_template("chat.html", username=username, messages=messages)
 
 if __name__ == "__main__":
     webapp.run(use_reloader=True)
