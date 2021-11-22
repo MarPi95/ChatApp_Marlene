58cc65288c8b464829d64a78ca125dba4641aa19
Marlene Pirker <mar.pirker@hotmail.com>
2021-11-22 20:35:07 +0100

    Lesson 21 -1

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import hashlib
 import os
 import uuid
-import hashlib
+
 from flask import Flask, render_template, request, make_response
 from flask_sqlalchemy import SQLAlchemy
 
 webapp = Flask(__name__)
-webapp.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
+webapp.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3").replace("postgres://", "postgresql://", 1)
 webapp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
@@ -16,15 +17,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=False, nullable=False)
     password = db.Column(db.String, nullable=False)
-    messages = db.relationship('Message', backref='user', lazy=True) #one to many, 1 user mehrere Messages, lazyloading = erst wenn geladen und verwendet dann wird es geladen
-    session_token = db.Column(db.String, unique=True)
+    deleted = db.Column(db.Boolean, nullable=False)
+    messages = db.relationship('Message', backref='user', lazy=True)
+    session_token = db.Column(db.String, unique=False)
+
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
+
 db.create_all()
+
 
 @webapp.route("/", methods=["GET"])
 def index():
@@ -32,21 +37,26 @@ def index():
 
     return render_template("index.html", username=username)
 
+
 @webapp.route("/login", methods=["Post"])
 def login():
     username = request.form.get("username")
     password = request.form.get("password")
+
     remember_me = request.form.get("remember")
 
     user_model = db.session.query(User).filter(User.username == username).first()
     if not user_model:
-        user_model = User(username=username, password=hashlib.sha256(password.encode()).hexdigest())
+        user_model = User(username=username, deleted=False, password=hashlib.sha256(password.encode()).hexdigest())
         db.session.add(user_model)
         db.session.commit()
 
+    if user_model.deleted:
+        return render_template("index.html", error="No active user!")
+
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     if hashed_password != user_model.password:
-        return render_template("index.html", error="Wrong username/password! :(")
+        return render_template("index.html", error="Wrong username/password!")
     else:
         session_token = str(uuid.uuid4())
 
@@ -62,59 +72,139 @@ def login():
         response.set_cookie("remember_user", "")
 
     response.set_cookie("session_token", session_token, httponly=True, samesite='Strict')
+
     return response
+
 
 @webapp.route("/logout", methods=["GET"])
 def logout():
     session_token = request.cookies.get("session_token")
-    response = make_response(render_template("index.html", error="User is not logged in."))
+    if not session_token:
+        return render_template("index.html", error="User is not logged in!")
+
+    response = make_response(render_template("index.html"))
     if session_token:
         response.set_cookie("session_token", "")
+
     return response
 
-@webapp.route("/chat", methods=["GET", "POST"])
-def chat():
-    session_token = request.cookies.get("session_token")
 
-    if not session_token:
-        return render_template("index.html", error="User is not logged in! :(")
-    username = None
+@webapp.route("/message", methods=["GET", "POST"])
+def chat():
+    user_model = check_user(request)
+    if not user_model:
+        return render_template("index.html", error="User is not logged in!")
+
     if request.method == "POST":
         message = request.form.get("message")
 
-        user_model = db.session.query(User).filter(User.session_token == session_token).one()
         message_model = Message(user_id=user_model.id, message=message)
 
         db.session.add(message_model)
         db.session.commit()
 
-        username = user_model.username
-
     messages = Message.query.all()
 
-    return render_template("chat.html", username=username, messages=messages)
+    return render_template("chat.html", username=user_model.username, messages=messages)
 
 
-@webapp.route("/delete", methods=["GET"])
-def delete():
-
-    session_token = request.cookies.get("session_token")
-
-    if not session_token:
-        return render_template("index.html", error="User is not logged in! :(")
-
-    message_id = request.args.get("id")
+@webapp.route("/message/delete/<message_id>", methods=["GET"])
+def delete(message_id):
+    user_model = check_user(request)
+    if not user_model:
+        return render_template("index.html", error="User is not logged in!")
 
     print("delete {}".format(message_id))
-    user_model = db.session.query(User).filter(User.session_token == session_token).one()
-    message_model = db.session.query(Message).filter(Message.id == message_id).first()
-    if message_id in user_model.messages:
-       db.session.delete(message_model)
-       db.session.commit()
+
+    message_model = db.session.query(Message).filter(Message.id == int(message_id)).first()
+
+    if message_model in user_model.messages:
+        db.session.delete(message_model)
+        db.session.commit()
 
     messages = Message.query.all()
 
     return render_template("chat.html", username=user_model.username, messages=messages)
+
+
+@webapp.route("/profile", methods=["GET"])
+def profile():
+    user_model = check_user(request)
+    if not user_model:
+        return render_template("index.html", error="User is not logged in!")
+
+    return render_template("profile.html", user=user_model)
+
+
+@webapp.route("/profile/edit", methods=["GET", "POST"])
+def profile_edit():
+    user_model = check_user(request)
+    if not user_model:
+        return render_template("index.html", error="User is not logged in!")
+
+    if request.method == "POST":
+        user_model.username = request.form.get("username")
+
+        db.session.add(user_model)
+        db.session.commit()
+
+        return render_template("profile.html", user=user_model)
+    else:
+        return render_template("profile_edit.html", user=user_model)
+
+
+@webapp.route("/profile/delete", methods=["GET"])
+def profile_delete():
+    user_model = check_user(request)
+    if not user_model:
+        return render_template("index.html", error="User is not logged in!")
+
+    user_model.deleted = True
+    db.session.add(user_model)
+    db.session.commit()
+
+    return render_template("index.html")
+
+
+@webapp.route("/user", methods=["GET"])
+def user():
+    user_model = check_user(request)
+    if not user_model:
+        return render_template("index.html", error="User is not logged in!")
+
+    users = User.query.all()
+
+    return render_template("user.html", user=user_model, users=users)
+
+
+@webapp.route("/user/<user_id>", methods=["GET"])
+def user_edit(user_id):
+    user_model = check_user(request)
+    if not user_model:
+        return render_template("index.html", error="User is not logged in!")
+
+    user_model_details = db.session.query(User).filter(User.id == int(user_id)).one()
+
+    return render_template("user_details.html", user=user_model, user_details=user_model_details)
+
+
+def check_session(request):
+    session_token = request.cookies.get("session_token")
+    if not session_token:
+        return None
+    return session_token
+
+
+def check_user(request):
+    session_token = check_session(request)
+    if not session_token:
+        return None
+
+    user_model = db.session.query(User).filter(User.session_token == session_token).one()
+    if user_model.deleted:
+        return None
+    return user_model
+
 
 if __name__ == "__main__":
     webapp.run(use_reloader=True)
